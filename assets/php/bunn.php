8c87d2c0d11c68dac8ef5e921137667b0ffb5938
Martin Koksrud Bekkelund <martin@Martins-MacBook-Pro.local>
2016-03-27 22:21:18 +0200

    Lagt inn URL til repo

--- assets/php/bunn.php
+++ assets/php/bunn.php
@@ -2,7 +2,7 @@
 </div> <!-- div#beholder -->
 
 <div id="bunn">
-	<p>Bygget med <a href="">Bærtur</a> av <a href="http://www.bekkelund.net/">Martin Koksrud Bekkelund</a></p>
+	<p>Bygget med <a href="https://github.com/MartinBekkelund/Baertur">Bærtur</a> av <a href="http://www.bekkelund.net/">Martin Koksrud Bekkelund</a></p>
 </div>
 
 </body>
